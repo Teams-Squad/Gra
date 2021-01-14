2a98902ceab1b2965ac4b5a807d9f70176e278a2
ksw0911 <ksw0911@wp.pl>
2021-01-14 19:07:59 +0100

    komunikat

--- Salon.cs
+++ Salon.cs
@@ -4,8 +4,39 @@ namespace Gra
 {
     public class Salon
     {
+        public static void barek()
+        {
+            ConsoleKeyInfo cki;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("######################################");
+                Console.WriteLine("#               BAREK                #");
+                Console.WriteLine("######################################");
+                Console.WriteLine("#                                    #");
+                Console.WriteLine("#     ZNAJDUJESZ SIĘ PRZY            #");
+                Console.WriteLine("#     OBIEKCIE BAREK, ZNAJDUJE SIĘ   #");
+                Console.WriteLine("#     TUTAJ KILKA PÓŁEK , SZAFKA     #");
+                Console.WriteLine("#     ORAZ MAŁE ZAMKNIĘTE PUDEŁKO    #");
+                Console.WriteLine("#                                    #");
+                Console.WriteLine("#                                    #"); 
+                Console.WriteLine("#          CO CHCESZ ZROBIĆ ?        #");
+                Console.WriteLine("#                                    #");
+                Console.WriteLine("#                                    #");
+                Console.WriteLine("#   [1] PRZESZUKAJ                   #"); 
+                Console.WriteLine("#                                    #");
+                Console.WriteLine("#   [2] POWRÓT                       #");
+                Console.WriteLine("#                                    #");
+                Console.WriteLine("#                                    #");
+                Console.WriteLine("######################################");
+                
+                cki = Console.ReadKey();
+            } while (cki.Key != ConsoleKey.D1);
+        }
         public static void glowny()
         {
+            ConsoleKeyInfo cki;
+
             Console.WriteLine("######################################");
             Console.WriteLine("#                SALON               #");
             Console.WriteLine("######################################");
@@ -18,7 +49,21 @@ namespace Gra
             Console.WriteLine("#   [5] KUCHNIA                      #");
             Console.WriteLine("#                                    #");
             Console.WriteLine("######################################");
-            Console.ReadKey();
+           
+            cki = Console.ReadKey();
+            if (cki.Key == ConsoleKey.D1) barek();
+            /*  if (cki.Key == ConsoleKey.D2) stojak.glowny();
+             if (cki.Key == ConsoleKey.D3) kanapa.glowny();
+
+
+             if (cki.Key == ConsoleKey.D4) korytarz.glowny();
+             if (cki.Key == ConsoleKey.D5) kuchnia.glowny();
+           */
+            Console.Clear();
+            
         }
+          
+            
+        
     }
 }
